34d21faee3023350cb3c2582e79f129e99409383
Doz D <120613725+dozd165@users.noreply.github.com>
2025-12-10 16:11:28 +0700

    Remove VOS server configurations from config.py

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,7 @@ import os
 import platform
 
 VOS_SERVERS = [
-    {"name": "VOS-01 (171.244.56.166)", "url": "http://171.244.56.166:3161/external_new/server/"},
-    {"name": "VOS-02 (171.244.56.167)", "url": "http://171.244.56.167:2718/external/server/"},
-    {"name": "VOS-03 (171.244.56.169)", "url": "http://171.244.56.169:8800/external/server/"},
-    {"name": "VOS-04 (116.103.109.198)", "url": "http://116.103.109.198:2427/external/server/"},
-    {"name": "VOS-05 (116.103.109.197)", "url": "http://116.103.109.197:2889/external_new/server/"},
-    {"name": "VOS-06 (171.244.4.40)", "url": "http://171.244.4.40:7553/external_new/server/"},
-    {"name": "VOS-07 (171.244.4.36)", "url": "http://171.244.4.36:7487/external_new/server/"},
+
 ]
 DEFAULT_TIMEOUT = 45
 DEFAULT_ENCODING = "utf-8"
